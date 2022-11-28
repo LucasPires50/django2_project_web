f81b3ef5832ad04323c2106648665b81be9717ae
LucasPires50 <lluc_50@outlook.com>
2022-11-28 11:31:06 -0300

    config: contato form

--- core/views.py
+++ core/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from .forms import ContatoForm
 
 def index(request):
     return render(request, 'index.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        print(f'Post: {request.POST}')
+
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            menssagem = form.cleaned_data['menssagem']
+
+            print('Menssagem enviada')
+            print(f'{nome} \n {email} \n {assunto} \n {menssagem}')
+
+            messages.success(request, "E-mail enviado com sucesso!")
+            form = ContatoForm()
+        else:
+            messages.error(request, "Error ao enviar o e-mail.")
+
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
 
 def produto(request):
     return render(request, 'produto.html')
