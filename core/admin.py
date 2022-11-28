10cb5f21dbd59feadfbd808259912e0291ba3478
LucasPires50 <lluc_50@outlook.com>
2022-11-28 16:10:50 -0300

    config: models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Produto
+
+"""
+decoreitor -> @admin.register(Produto)
+"""
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+  list_display = ('nome', 'preco', 'estoque', 'slug', 'criado', 'modificado', 'ativo')
