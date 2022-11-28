6a102c0004904b9fb0c83cae1acadfab4635a8ad
Lucas <luc_50@outlook.com>
2022-11-28 19:25:42 -0300

    adjust gitignore

--- django2/conf.py
+++
@@ -1,8 +0,0 @@
-# -- coding: utf-8 --
-
-# Configuração Banco de dados
-DEV_DB_NAME = "django2"
-DEV_DB_USER = "user_django2"
-DEV_DB_PASSWORD = "uD7Yobu?FP|G~74Y"
-DEV_DB_HOST = "database-1.cmxlaapu7sci.us-east-1.rds.amazonaws.com"
-DEV_DB_PORT = "3306"
